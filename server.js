cf8ca5860424e481a03cbfad1cabb9bdcbe22e1a
mor bargig <morbargig@gmail.com>
2019-11-22 08:42:23 +0200

    First commit

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+
+
+
+
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
+
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+
+
+
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
-app.listen(process.env.PORT || 3000);
+// app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+
+port = 8000
+
+
+
+app.listen(process.env.PORT || port, () => console.log(`Running server on port ` + port))
+
+
+
+
+
